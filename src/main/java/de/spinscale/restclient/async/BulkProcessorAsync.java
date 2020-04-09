5712d81ba373b8cf0af30da0bc040c2198626715
Alexander Reelsen <alexander@reelsen.net>
2020-04-09 20:16:15 +0200

    Implement bulk processor that returns an async future

---
+++ src/main/java/de/spinscale/restclient/async/BulkProcessorAsync.java
@@ -0,0 +1,115 @@
+package de.spinscale.restclient.async;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import de.spinscale.restclient.Product;
+import de.spinscale.restclient.ProductDeserializer;
+import de.spinscale.restclient.ProductSerializer;
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.support.PlainActionFuture;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * This is a bulk processor returning a PlainActionFuture
+ *
+ * Idea of this implementation is to use a bulk processor, when updating a lot of single documents
+ * This way you prevent those updates at the expense of increased wait times (up to one second maximum)
+ *
+ * Limitations of this is the fact that an id can only be written once at the same time,
+ * as we use the document id as a map key
+ */
+public class BulkProcessorAsync implements AutoCloseable {
+
+    private final BulkProcessor bulkProcessor;
+    private final String index;
+    private final ObjectMapper mapper;
+    private final Map<String, PlainActionFuture<IndexResponse>> futures;
+
+    public BulkProcessorAsync(RestHighLevelClient client, String index) {
+        this.bulkProcessor = BulkProcessor.builder(
+                (request, bulkListener) -> client.bulkAsync(request, RequestOptions.DEFAULT, bulkListener),
+                new FutureNotificationListener())
+                .setFlushInterval(TimeValue.timeValueSeconds(1))
+                .setBulkActions(100)
+                .build();
+        this.index = index;
+        this.mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Product.class, new ProductSerializer());
+        module.addDeserializer(Product.class, new ProductDeserializer());
+        mapper.registerModule(module);
+        futures = new ConcurrentHashMap<>();
+    }
+
+    public PlainActionFuture<IndexResponse> save(Product product) throws Exception {
+        Objects.requireNonNull(product.getId(), "ID cannot be null");
+        final PlainActionFuture<IndexResponse> future = PlainActionFuture.newFuture();
+        final IndexRequest indexRequest = indexRequest(product);
+        final PlainActionFuture<IndexResponse> existingFuture = futures.putIfAbsent(indexRequest.id(), future);
+        if (existingFuture != null) {
+            future.onFailure(new RuntimeException("product[" + product.getId() + "] is already being stored"));
+        } else {
+            bulkProcessor.add(indexRequest);
+        }
+        return future;
+    }
+
+    private IndexRequest indexRequest(Product product) throws IOException {
+        final byte[] bytes = mapper.writeValueAsBytes(product);
+        final IndexRequest request = new IndexRequest(index);
+        if (product.getId() != null) {
+            request.id(product.getId());
+        }
+        request.source(bytes, XContentType.JSON);
+        return request;
+    }
+
+    @Override
+    public void close() {
+        bulkProcessor.close();
+    }
+
+    public class FutureNotificationListener implements BulkProcessor.Listener {
+
+        @Override
+        public void beforeBulk(long executionId, BulkRequest request) {
+            System.out.println("BEFORE BULK");
+        }
+
+        @Override
+        public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+            System.out.println("AFTER BULK");
+            System.out.println(request.requests());
+            System.out.println(response.getItems()[0].status());
+            System.out.println(futures.keySet());
+
+            for (int i = 0; i < request.numberOfActions(); i++) {
+                final PlainActionFuture<IndexResponse> future = futures.remove(request.requests().get(i).id());
+                final IndexResponse indexResponse = response.getItems()[i].getResponse();
+                future.onResponse(indexResponse);
+            }
+        }
+
+        @Override
+        public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+            failure.printStackTrace();
+            System.out.println("AFTER BULK2");
+            for (int i = 0; i < request.numberOfActions(); i++) {
+                final PlainActionFuture<IndexResponse> future = futures.remove(request.requests().get(i).id());
+                future.onFailure(new Exception(failure));
+            }
+        }
+    }
+}
---
+++ src/test/java/de/spinscale/restclient/async/BulkProcessorAsyncTests.java
@@ -0,0 +1,78 @@
+package de.spinscale.restclient.async;
+
+import de.spinscale.restclient.Product;
+import org.apache.http.HttpHost;
+import org.elasticsearch.ElasticsearchStatusException;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.support.PlainActionFuture;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.rest.RestStatus;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// test class for the just for fun bulk processor
+class BulkProcessorAsyncTests {
+
+    private static final ElasticsearchContainer container =
+            new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:7.6.2").withExposedPorts(9200);
+
+    private static final String INDEX = "my_index";
+    private static RestHighLevelClient client;
+    private static BulkProcessorAsync bulkProcessorAsync;
+
+    @BeforeAll
+    public static void startElasticsearchCreateLocalClient() {
+        container.start();
+
+        HttpHost host = new HttpHost("localhost", container.getMappedPort(9200));
+        final RestClientBuilder builder = RestClient.builder(host);
+        client = new RestHighLevelClient(builder);
+        bulkProcessorAsync = new BulkProcessorAsync(client, INDEX);
+    }
+
+    @AfterAll
+    public static void closeResources() throws Exception {
+        client.close();
+        bulkProcessorAsync.close();
+    }
+
+    @AfterEach
+    public void deleteProductIndex() throws Exception {
+        try {
+            client.indices().delete(new DeleteIndexRequest(INDEX), RequestOptions.DEFAULT);
+        } catch (ElasticsearchStatusException e) {
+        }
+    }
+
+    @Test
+    public void testAsyncSave() throws Exception {
+        Product product = new Product();
+        product.setId("my_id");
+        product.setName("my_name");
+        product.setDescription("my fancy description");
+        product.setPrice(12.34);
+        product.setStockAvailable(1234);
+        final PlainActionFuture<IndexResponse> future = bulkProcessorAsync.save(product);
+        final IndexResponse indexResponse = future.get(10, TimeUnit.SECONDS);
+
+        assertThat(indexResponse.status()).isEqualTo(RestStatus.CREATED);
+
+        // retrieve document
+        final GetResponse getResponse = client.get(new GetRequest(INDEX, "my_id"), RequestOptions.DEFAULT);
+        assertThat(getResponse.isExists()).isTrue();
+    }
+
+}
